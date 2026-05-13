436f9d5aece52d6cd6a3427dd218e421131d089f
So Kuroki <sokuroki@sakana.ai>
2026-05-13 01:05:48 +0000

    copyright

--- src/kame/server_oracle_parallel.py
+++ src/kame/server_oracle_parallel.py
@@ -1,7 +1,3 @@
-# Copyright (c) Kyutai, all rights reserved.
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
 import argparse
 import asyncio
 import collections
